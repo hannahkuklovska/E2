44ea8b4a60d5ebd0ba8339d36ecc536455cc0253
hannahkuklovska <hannah15072002@gmail.com>
2024-05-12 09:26:36 +0200

    priradovanie elementu matice

--- maticova_rovnica.c
+++ maticova_rovnica.c
@@ -14,4 +14,5 @@ MAT *mat_create_with_type(unsigned int rows, unsigned int cols)
      MAT *mat = (MAT *)malloc(sizeof(MAT));
      mat->rows = rows;
      mat->cols = cols;
+     mat->elem = (float *)malloc(rows * cols * sizeof(float));
 }
